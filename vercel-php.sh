cb4f8ff33f09abc97e23a6da0cfe4ae40898f11b
muhammad aiz nur aziz <nmuhammadaiz@gmail.com>
2026-03-07 16:59:09 +0700

    fix: download composer.phar manually

--- vercel-php.sh
+++ vercel-php.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cd backend && composer install --no-dev --prefer-dist --optimize-autoloader
+
+# 1. Unduh composer.phar secara manual ke folder root
+curl -sS https://getcomposer.org/installer | php
+
+# 2. Pindah ke folder backend
+cd backend
+
+# 3. Jalankan instalasi menggunakan php ../composer.phar
+php ../composer.phar install --no-dev --prefer-dist --optimize-autoloader
